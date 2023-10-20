b2a86c2841af3edb7b8ed8baa8c491aed6c0ca59
jpcc0 <conleyjoshuakm@gmail.com>
2023-10-19 23:01:54 -0400

    npm updates and styling issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    extend: {
+      // backgroundImage: theme => ({
+      //   ...theme('backgroundImage'),
+      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      //   'gradient-conic': 'conic-gradient(var(--tw-gradient-stops))',
+      // }),
+    },
+  variants: {
     extend: {},
   },
   plugins: [],
-};
+  }
+}
