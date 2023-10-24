616543583e6c46e75914b56d9cd43ca9c90d3434
jpcc0 <conleyjoshuakm@gmail.com>
2023-10-23 21:53:56 -0400

    typewriter addition for loading screen

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
+  plugins: [
+    require('postcss-nested'), // <-- This should come before Tailwind
+    require('tailwindcss'),
+    autoprefixer, {},
+  ],
+  }
