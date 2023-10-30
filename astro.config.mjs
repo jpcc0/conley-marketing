7cc1d7648d7ea30f49a862564d271069c1fb3d1d
jpcc0 <conleyjoshuakm@gmail.com>
2023-10-30 14:09:33 -0400

    added svelte for forms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import svelte from '@astrojs/svelte';
 
 
 export default defineConfig({
   // ...
-  integrations: [tailwind()],
+  integrations: [tailwind(), svelte()],
   //             ^^^^^^^^^^
 });
